c9e3d5f41625815412b7de1e9c11bd8076416106
John Watson <johnwatson484@hotmail.com>
2021-08-22 19:43:28 +0100

    Syntax

--- app/inbound/save-invoice-lines.js
+++ app/inbound/save-invoice-lines.js
@@ -2,7 +2,7 @@ const db = require('../data')
 
 const saveInvoiceLines = async (invoiceLines, paymentRequestId, transaction) => {
   for (const invoiceLine of invoiceLines) {
-    await db.invoiceLine.create({paymentRequestId, ...invoiceLine}, {transaction})
+    await db.invoiceLine.create({ paymentRequestId, ...invoiceLine }, { transaction })
   }
 }
 
