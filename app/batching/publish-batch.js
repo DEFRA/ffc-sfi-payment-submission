8ed032c6d0022dfafeaa9488b6c25e64a42a4b0d
John Watson <johnwatson484@hotmail.com>
2021-09-02 10:14:42 +0100

    Remove redundant function

--- app/batching/publish-batch.js
+++ app/batching/publish-batch.js
@@ -2,14 +2,10 @@ const { getOutboundBlobClient } = require('../storage')
 const { writeToString } = require('@fast-csv/format')
 
 const publishBatch = async (filename, content) => {
-  const body = await stringifyContent(content)
+  const body = await writeToString(content)
   const outboundBlobClient = await getOutboundBlobClient(filename)
   await outboundBlobClient.upload(body, body.length)
   console.info(`Published ${filename}`)
-}
-
-const stringifyContent = async (content) => {
-  return writeToString(content)
 }
 
 module.exports = publishBatch
