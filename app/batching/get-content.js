7575121ddb213791a3f4cfc625e67fc1c5a73473
John Watson <42574977+johnwatson484@users.noreply.github.com>
2022-04-07 12:25:16 +0100

    Remove vendor grouping from AR (#29)

--- app/batching/get-content.js
+++ app/batching/get-content.js
@@ -22,13 +22,11 @@ const getAPContent = (paymentRequest, batch) => {
 }
 
 const getARContent = (paymentRequest, batch) => {
+  const valueLine = paymentRequest.value > 0 ? getHighestValueLine(paymentRequest.invoiceLines) : getLowestValueLine(paymentRequest.invoiceLines)
   const rows = []
-  const vendorGroups = getVendorGroups(paymentRequest.invoiceLines)
-  for (const vendorGroup of vendorGroups) {
-    rows.push(getVendorLineAR(paymentRequest, vendorGroup, batch))
-    for (const [lineId, invoiceLine] of vendorGroup.invoiceLines.entries()) {
-      rows.push(getLedgerLineAR(invoiceLine, paymentRequest, lineId + 1))
-    }
+  rows.push(getVendorLineAR(paymentRequest, batch, valueLine))
+  for (const [lineId, invoiceLine] of paymentRequest.invoiceLines.entries()) {
+    rows.push(getLedgerLineAR(invoiceLine, paymentRequest, lineId + 1))
   }
   return rows
 }
@@ -37,18 +35,8 @@ const getHighestValueLine = (invoiceLines) => {
   return invoiceLines.reduce((prev, current) => (prev.value > current.value) ? prev : current)
 }
 
-const getVendorGroups = (invoiceLines) => {
-  return [...invoiceLines.reduce((x, y) => {
-    // group by scheme and fund, so create key representing the combination
-    const key = `${y.schemeCode}-${y.fundCode}`
-
-    // if key doesn't exist then first instance so create new group
-    const item = x.get(key) || Object.assign({}, { fundCode: y.fundCode, schemeCode: y.schemeCode, value: 0, invoiceLines: [] })
-    item.value += Number(y.value)
-    item.invoiceLines.push(y)
-
-    return x.set(key, item)
-  }, new Map()).values()]
+const getLowestValueLine = (invoiceLines) => {
+  return invoiceLines.reduce((prev, current) => (prev.value < current.value) ? prev : current)
 }
 
 module.exports = getContent
