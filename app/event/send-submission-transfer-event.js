225d79457ebde0ea97771006ae3fadbf24fac8b8
John Watson <johnwatson484@hotmail.com>
2022-04-14 07:52:22 +0100

    Switch to batch events

--- app/event/send-submission-transfer-event.js
+++ app/event/send-submission-transfer-event.js
@@ -1,20 +1,16 @@
-const raiseEvent = require('./raise-event')
+const raiseEvents = require('./raise-events')
 const { v4: uuidv4 } = require('uuid')
 
 const sendSubmissionTransferEvent = async (filename, batch) => {
-  const batchId = batch.batchId
-  const ledger = batch.ledger
-  for (const paymentRequest of batch.paymentRequests) {
-    const correlationId = paymentRequest?.correlationId ?? uuidv4()
-    const event = {
-      id: correlationId,
-      name: 'payment-request-submission',
-      type: 'info',
-      message: 'Payment request submission scheduled',
-      data: { batchId, filename, ledger }
-    }
-    await raiseEvent(event)
-  }
+  const events = batch.paymentRequests.map(paymentRequest => ({
+    id: paymentRequest.correlationId ?? uuidv4(),
+    name: 'payment-request-submission',
+    type: 'info',
+    message: 'Payment request submission scheduled',
+    data: { batchId: batch.batchId, filename, ledger: batch.ledger }
+
+  }))
+  await raiseEvents(events)
 }
 
 module.exports = sendSubmissionTransferEvent
