9560fa64e89513b8b5af618e2fa12b43c99af54a
John Watson <johnwatson484@hotmail.com>
2023-02-17 23:13:19 +0000

    Update

--- app/event/send-submission-transfer-event.js
+++ app/event/send-submission-transfer-event.js
@@ -1,7 +1,18 @@
-const raiseEvents = require('./raise-events')
+const { EventPublisher } = require('ffc-pay-event-publisher')
 const { v4: uuidv4 } = require('uuid')
+const raiseEvents = require('./raise-events')
+const config = require('../config')
 
 const sendSubmissionTransferEvent = async (filename, batch) => {
+  if (config.useV1Events) {
+    await sendV1SubmissionTransferEvent(filename, batch)
+  }
+  if (config.useV2Events) {
+    await sendV2SubmissionTransferEvent(filename, batch)
+  }
+}
+
+const sendV1SubmissionTransferEvent = async (filename, batch) => {
   const events = batch.paymentRequests.map(paymentRequest => ({
     id: paymentRequest.correlationId ?? uuidv4(),
     name: 'payment-request-submission',
@@ -11,6 +22,20 @@ const sendSubmissionTransferEvent = async (filename, batch) => {
 
   }))
   await raiseEvents(events)
+}
+
+const sendV2SubmissionTransferEvent = async (filename, batch) => {
+  const event = {
+    source: 'ffc-pay-batch-processor',
+    type: 'uk.gov.defra.ffc.pay.batch.submitted.dax',
+    subject: filename,
+    data: {
+      filename,
+      ...batch
+    }
+  }
+  const eventPublisher = new EventPublisher(config.eventsTopic)
+  await eventPublisher.publishEvent(event)
 }
 
 module.exports = sendSubmissionTransferEvent
