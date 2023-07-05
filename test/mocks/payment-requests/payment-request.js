2e5c7f790a7a5901822d2d0221046ebc65f4769e
John Watson <john.watson1@defra.gov.uk>
2023-07-05 08:50:03 +0100

    Add tests

--- test/mocks/payment-requests/payment-request.js
+++ test/mocks/payment-requests/payment-request.js
@@ -9,6 +9,8 @@ const { DELIVERY_BODY_RPA } = require('../values/delivery-body')
 const { SFI_INVOICE_NUMBER } = require('../values/invoice-number')
 const { FRN } = require('../values/frn')
 const { SBI } = require('../values/sbi')
+const { VENDOR } = require('../values/vendor')
+const { TRADER } = require('../values/trader')
 const { PAYMENT_REQUEST_NUMBER } = require('../values/payment-request-number')
 const { AGREEMENT_NUMBER } = require('../values/agreement-number')
 const { CONTRACT_NUMBER } = require('../values/contract-number')
@@ -26,6 +28,8 @@ module.exports = {
   invoiceNumber: SFI_INVOICE_NUMBER,
   frn: FRN,
   sbi: SBI,
+  vendor: VENDOR,
+  trader: TRADER,
   paymentRequestNumber: PAYMENT_REQUEST_NUMBER,
   agreementNumber: AGREEMENT_NUMBER,
   contractNumber: CONTRACT_NUMBER,
