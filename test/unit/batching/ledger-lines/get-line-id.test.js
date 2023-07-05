c1807c6e9f0009daec90d66d7cf7700374d7e04e
John Watson <john.watson1@defra.gov.uk>
2023-07-05 09:20:40 +0100

    Add tests

---
+++ test/unit/batching/ledger-lines/get-line-id.test.js
@@ -0,0 +1,19 @@
+const { ES, IMPS, SFI } = require('../../../../app/constants/schemes')
+
+const { getLineId } = require('../../../../app/batching/ledger-lines/get-line-id')
+
+const lineId = 1
+
+describe('get line id', () => {
+  test('returns empty string for ES', () => {
+    expect(getLineId(ES, lineId)).toBe('')
+  })
+
+  test('returns empty string for IMPS', () => {
+    expect(getLineId(IMPS, lineId)).toBe('')
+  })
+
+  test('returns 1 for other schemes', () => {
+    expect(getLineId(SFI, lineId)).toBe(1)
+  })
+})
