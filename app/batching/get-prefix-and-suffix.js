31ad4c3f7debe02d42e4aae7675705625ca82f42
Sam Plackett <samuel.plackett@eviden.com>
2023-10-10 10:04:22 +0100

    add pfx sfx failsafe

--- app/batching/get-prefix-and-suffix.js
+++ app/batching/get-prefix-and-suffix.js
@@ -2,6 +2,9 @@ const batchProperties = require('../constants/batch-properties')
 
 const getPrefixAndSuffix = (schemeId) => {
   const schemeIdx = batchProperties.findIndex(scheme => scheme.schemeId === schemeId)
+  if (schemeIdx === -1) {
+    return ['', '']
+  }
   const prefix = batchProperties[schemeIdx].prefix
   const suffix = batchProperties[schemeIdx].suffix
   return [prefix, suffix]
