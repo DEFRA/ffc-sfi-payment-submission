1146d57506c17ada960e687863ed4bb25b106d5c
Harsh Vasudev <harsh.vasudev.external@eviden.com>
2024-10-02 22:11:58 +0100

    DOC-583 : using appinsight connection string

--- app/insights.js
+++ app/insights.js
@@ -1,14 +1,18 @@
 const appInsights = require('applicationinsights')
 
-function setup () {
-  if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
-    appInsights.setup().start()
+const setup = () => {
+  if (process.env.APPINSIGHTS_CONNECTIONSTRING) {
+    appInsights.setup(process.env.APPINSIGHTS_CONNECTIONSTRING)
+      .setAutoCollectDependencies(false)
+      .start()
     console.log('App Insights Running')
     const cloudRoleTag = appInsights.defaultClient.context.keys.cloudRole
     const appName = process.env.APPINSIGHTS_CLOUDROLE
-    appInsights.defaultClient.context.tags[cloudRoleTag] = appName
+
+    const client = appInsights.defaultClient
+    client.context.tags[cloudRoleTag] = appName
   } else {
-    console.log('App Insights Not Running!')
+    console.log('App Insights Not Running')
   }
 }
 
